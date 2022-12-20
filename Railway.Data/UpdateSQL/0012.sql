621bbf019c1371c2c7ddcde180e3ee024f322711
Владимир Андреев <vladimir_andreev@rambler.ru>
2022-12-20 13:47:16 +0300

    Упростил процедуру

---
+++ Railway.Data/UpdateSQL/0012.sql
@@ -0,0 +1,44 @@
+﻿-- допустим мы уже получили идентификатор пассажира
+
+ALTER procedure [ticketservice].[insert_ticket]
+
+	@train_id		int,
+	@car_number		int,
+	@seat_number	int,
+	@this_boarding_st_id		int,
+	@this_destination_st_id		int,
+	@passanger	nvarchar(50)
+as
+begin
+
+	declare @seat_id int;		
+	SELECT @seat_id = s.id  FROM seat s
+	WHERE s.train_id = @train_id 
+		and s.car_number = @car_number 
+		and s.seat_number = @seat_number
+
+	IF  EXISTS( SELECT 1 FROM TICKET t1 where t1.train_id = @train_id and t1.seat_id = @seat_id)
+	BEGIN
+		return;
+	END
+	ELSE
+	BEGIN
+
+		insert into TICKET(
+			number,
+			train_id,
+			seat_id,
+			boarding_st_id,
+			destination_st_id,
+			passanger
+		)
+		values(
+			6000,
+			@train_id,
+			@seat_id,
+			@this_boarding_st_id,
+			@this_destination_st_id,
+			@passanger
+		)
+	END
+end
