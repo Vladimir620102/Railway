6d33083c970c57bd9a37778900dc759d4adbeca5
Владимир Андреев <vladimir_andreev@rambler.ru>
2022-12-23 21:32:28 +0300

    CAR_TYPE

---
+++ Railway.Data/UpdateSQL/0022.sql
@@ -0,0 +1,7 @@
+﻿DELETE FROM [CAR_TYPE]
+SET IDENTITY_INSERT [CAR_TYPE] ON
+INSERT INTO [CAR_TYPE] (id, [name], capacity) VALUES (1, 'О', 81)
+INSERT INTO [CAR_TYPE] (id, [name], capacity) VALUES (2, 'П', 54)
+INSERT INTO [CAR_TYPE] (id, [name], capacity) VALUES (3, 'К', 36)
+INSERT INTO [CAR_TYPE] (id, [name], capacity) VALUES (4, 'СВ', 18)
+SET IDENTITY_INSERT [CAR_TYPE] OFF
