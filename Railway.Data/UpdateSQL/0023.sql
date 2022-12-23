38d43dd993cd0214072dc9000cd625837bdd98e1
Владимир Андреев <vladimir_andreev@rambler.ru>
2022-12-23 21:45:35 +0300

    1

---
+++ Railway.Data/UpdateSQL/0023.sql
@@ -0,0 +1,20 @@
+﻿ALTER PROCEDURE [dbo].[delete_train] @train_id int
+AS
+BEGIN
+	SET NOCOUNT ON;
+	-- удалить места
+	-- удалить вагоны
+
+	delete s
+	FROM SEAT as s
+	WHERE s.train_id = @train_id
+
+	delete s
+	from STOCK as s
+	where s.train_id=@train_id
+
+	delete tr
+	from TRAIN tr
+	where tr.id = @train_id
+    
+END
